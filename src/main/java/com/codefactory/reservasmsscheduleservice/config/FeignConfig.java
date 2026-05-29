2c0a841de3d062908d62d86f227d04d835e333ac
CodeInTheIzzyverse <dev.izzyverse@gmail.com>
2026-05-29 17:09:08 -0500

    AsyncConfig+ FeignConfig + Connection Pool

--- src/main/java/com/codefactory/reservasmsscheduleservice/config/FeignConfig.java
+++ src/main/java/com/codefactory/reservasmsscheduleservice/config/FeignConfig.java
@@ -2,6 +2,12 @@ package com.codefactory.reservasmsscheduleservice.config;
 
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import feign.httpclient.ApacheHttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -20,5 +26,25 @@ public class FeignConfig implements RequestInterceptor {
                 template.header("Authorization", authHeader);
             }
         }
+    }
+
+    @Bean
+    public ApacheHttpClient httpClient() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(50);
+        connectionManager.setDefaultMaxPerRoute(10);
+
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectionRequestTimeout(5000)
+                .setSocketTimeout(10000)
+                .setConnectTimeout(5000)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new ApacheHttpClient(httpClient);
     }
 }
